6ccd98506e8d71016badea5167495484644c4f56
Oleksii Hlaholiev <oleksii.hlaholiev@gmail.com>
2016-10-11 19:53:49 +0300

    initial commit

---
+++ landing-page-ham/js/mainScript.js
@@ -0,0 +1,20 @@
+'use strict';
+
+window.addEventListener('load', function() {
+
+    /*var sectionHeader = document.getElementById("header");
+    var sectionHome = document.getElementById("home");
+    var sectionServices = document.getElementById("services");
+    var sectionPortfolio = document.getElementById("portfolio");
+    var sectionAbout = document.getElementById("about");
+    var sectionNews = document.getElementById("news");
+    var sectionContacts = document.getElementById("contacts");
+
+    var logoButton = document.querySelector(".logo-button");
+    var topMenu = document.querySelectorAll(".top-menu > ul > li > a");
+    var sliderButton = document.querySelector(".slider-button");*/
+
+
+
+});
+
