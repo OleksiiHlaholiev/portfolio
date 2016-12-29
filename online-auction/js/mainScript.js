90af5fec8d5a58320fc7ba7ddcfe924be272a6b3
Oleksii Hlaholiev <oleksii.hlaholiev@gmail.com>
2016-12-29 22:08:54 +0200

    initial commit

---
+++ online-auction/js/mainScript.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// **** polyfills for IE
+/*if (!('remove' in Element.prototype)) {
+    Element.prototype.remove = function() {
+        if (this.parentNode) {
+            this.parentNode.removeChild(this);
+        }
+    };
+}*/
+// ****************************************
+
+window.addEventListener('load', function() {
+
+
+    <!--PRELOADER !!!-->
+    /*setTimeout(function() {
+        $('#preloader').fadeOut('slow', function() {});
+    }, 2000);*/
+
+});
+
