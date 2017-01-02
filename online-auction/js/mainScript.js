1b91dc7deecbf100f9cf4fa2a01934195219410f
Oleksii Hlaholiev <oleksii.hlaholiev@gmail.com>
2017-01-02 21:52:31 +0200

    update

--- online-auction/js/mainScript.js
+++ online-auction/js/mainScript.js
@@ -17,6 +17,43 @@ window.addEventListener('load', function() {
     /*setTimeout(function() {
         $('#preloader').fadeOut('slow', function() {});
     }, 2000);*/
+    /*var overlayDiv = document.getElementsByClassName("overlay");
+
+    function overlayDivHandler() {
+		//onClick="style.pointerEvents='none'">
+		this.style.pointerEvents='none';
+	}
+
+
+	overlayDiv[0].addEventListener("click", overlayDivHandler);*/
+
+    // ****************************************************************
+	// Disable scroll zooming and bind back the click event
+	function onMapMouseleaveHandler (event) {
+		var that = this;
+
+		that.addEventListener('click', onMapClickHandler);
+		that.removeEventListener('mouseleave', onMapMouseleaveHandler);
+		that.querySelector('iframe').style.pointerEvents = "none";
+	}
+
+	function onMapClickHandler (event) {
+		var that = this;
+
+		// Disable the click handler until the user leaves the map area
+		that.removeEventListener('click', onMapClickHandler);
+
+		// Enable scrolling zoom
+		that.querySelector('iframe').style.pointerEvents = "auto";
+
+		// Handle the mouse leave event
+		that.addEventListener('mouseleave', onMapMouseleaveHandler);
+	}
+
+	// Enable map zooming with mouse scroll when the user clicks the map
+	$('.map-cont')[0].addEventListener('click', onMapClickHandler);
+
+
 
 });
 
