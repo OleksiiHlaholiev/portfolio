4f95ee61d9f29f965d91036d5640e5e124218c40
Oleksii Hlaholiev <oleksii.hlaholiev@gmail.com>
2017-02-03 18:59:23 +0200

    update

--- koloro-test-project/js/mainScript.js
+++ koloro-test-project/js/mainScript.js
@@ -16,13 +16,23 @@ window.addEventListener('load', function() {
 		userActiveControlFlag = false,
 		busyFlag = false;
 
+	var backToTopBtn = document.querySelector(".back_to_top"),
+		sectionHeader = document.getElementById("header"),
+		sectionPortfolio = document.getElementById("portfolio"),
+		subSectionCompanyAims = document.querySelector(".company_aims");
+
+	var mainMenu = document.querySelector(".main_menu"),
+		menuTrigerBtn = document.querySelector(".menu_triger");
+
 	var slider = document.querySelector(".slider"),
 		sliderBtnLeft = document.querySelector(".fa-angle-left"),
 		sliderBtnRight = document.querySelector(".fa-angle-right"),
-		sliderItems = document.querySelectorAll(".slider-item");
+		sliderItems = document.querySelectorAll(".slider-item"),
+		sliderPaginationBtns = document.querySelectorAll(".pagination-btn");
 
 	// --------------------------- MAIN CODE --------------------------------------
 
+	scrollWindowHandler(); // initial call!!!
 
 	// initial call
 	var launchAutoSliderTimer = setTimeout(
@@ -34,6 +44,63 @@ window.addEventListener('load', function() {
 	);
 
 	// ************************* FUNCTIONS ***************************
+
+	function scrollFunc(startPos, stopPos, step) {
+		var epsilon = 0.1,
+			step_ms = 10,
+			deltaScroll = (stopPos - startPos) / step;
+
+		if (Math.abs(deltaScroll) > epsilon) {
+			scrollFuncTimer = setInterval(
+				function () {
+					// document.body.scrollTop
+					if (deltaScroll == 0 ||
+						(deltaScroll > 0 && pageYOffset >= stopPos) ||
+						(deltaScroll < 0 && pageYOffset <= stopPos)) {
+						clearInterval(scrollFuncTimer);
+						busyFlag = false;
+					} else {
+						scrollBy(0, deltaScroll);
+					}
+				},
+				step_ms
+			);
+		}
+		else {
+			busyFlag = false;
+		}
+	}
+
+	function myFadeIn(elementDOM, timeStep) {
+		var tempOpacity = 0;
+		var localTimer = setInterval(
+			function () {
+				tempOpacity += 1;
+				if (tempOpacity <= 100) {
+					elementDOM.style.opacity = String(tempOpacity / 100);
+				} else {
+					clearInterval(localTimer);
+				}
+			},
+			timeStep
+		);
+	}
+
+	function myFadeOut(elementDOM, timeStep) {
+		var tempOpacity = 100;
+		var localTimer = setInterval(
+			function () {
+				tempOpacity -= 1;
+				if (tempOpacity >= 0) {
+					elementDOM.style.opacity = String(tempOpacity / 100);
+				} else {
+					clearInterval(localTimer);
+					elementDOM.style.display = "none";
+				}
+			},
+			timeStep
+		);
+	}
 
 	function sliderRotate(direction) {
 		if (!sliderBusyFlag) {
@@ -56,7 +123,7 @@ window.addEventListener('load', function() {
 			for (var i = 0; i < sliderItems.length; i++) {
 				temp = getComputedStyle(sliderItems[i]).left;
 				temp = Number(temp.replace("px", "")) * 100 / sliderWidth + deltaX;
-				sliderItems[i].style.transition = "left 1s";
+				sliderItems[i].style.transition = "all 1s";
 				if (!Math.round(temp)) {
 					temp = 0;
 				}
@@ -106,6 +173,42 @@ window.addEventListener('load', function() {
 
 	// **********************  EVENT HANDLERS *************************
 
+	function scrollWindowHandler(event) {
+		var	tempOffset = 2 * sectionHeader.clientHeight,
+			currentPosition = document.body.scrollTop ?
+								(document.body.scrollTop + tempOffset) :
+								(document.documentElement.scrollTop + tempOffset);
+
+		if (currentPosition > sectionPortfolio.offsetTop + subSectionCompanyAims.offsetTop) {
+			if (!backToTopBtn.classList.contains("back_to_top_visible")) {
+				backToTopBtn.classList.add("back_to_top_visible");
+				// backToTopBtn.style.display = "block";
+			}
+
+		} else {
+			if (backToTopBtn.classList.contains("back_to_top_visible")) {
+				backToTopBtn.classList.remove("back_to_top_visible");
+				// backToTopBtn.style.display = "none";
+			}
+		}
+	}
+
+	function menuTrigerBtnHandler() {
+		mainMenu.classList.toggle("show_menu");
+	}
+
+	function backToTopBtnHandler(event) {
+		event.preventDefault();
+		if (!busyFlag) {
+			busyFlag = true;
+			scrollFunc(
+				pageYOffset,
+				sectionPortfolio.offsetTop,
+				SCROLL_STEP
+			)
+		}
+	}
+
 	function sliderBtnHandler(event) {
 		userActiveControlFlag = true;
 		clearTimeout(userTimoutTimer);
@@ -132,12 +235,63 @@ window.addEventListener('load', function() {
 		);
 	}
 
+	function sliderPaginationBtnHandler() {
+
+		if (!this.classList.contains("active")) {
+			var prevActivePaginationBtn = document.querySelector(".slider-nav-list li .active");
+
+			if (prevActivePaginationBtn.classList.contains("active")) {
+				prevActivePaginationBtn.classList.remove("active");
+			}
+
+			this.classList.add("active");
+		}
+
+
+
+
+		/*userActiveControlFlag = true;
+		clearTimeout(userTimoutTimer);
+		clearInterval(launchAutoSliderTimer);
+
+		if (event.target.classList.contains("fa-angle-left")) {
+			sliderRotate("left");
+		} else {
+			sliderRotate("right");
+		}
+
+
+
+		userTimoutTimer = setTimeout(
+			function () {
+				launchAutoSliderTimer = setTimeout(
+					function readyGo() {
+						userActiveControlFlag = false;
+						sliderRotate("right");
+						launchAutoSliderTimer = setTimeout(readyGo, SLIDER_AUTO_DELAY);
+					},
+					SLIDER_AUTO_DELAY
+				);
+			},
+			SLIDER_USER_TIMEOUT
+		);*/
+	}
+
 	// ************************************************************************************
 
 	// ***************** REGISTER EVENT HANDLERS *******************
 
+	menuTrigerBtn.addEventListener('click', menuTrigerBtnHandler);
+	backToTopBtn.addEventListener('click', backToTopBtnHandler);
+
 	sliderBtnLeft.addEventListener('click', sliderBtnHandler);
 	sliderBtnRight.addEventListener('click', sliderBtnHandler);
+
+	for (var i = 0; i < sliderPaginationBtns.length; i++) {
+		sliderPaginationBtns[i].addEventListener('click', sliderPaginationBtnHandler);
+	}
+
+	window.addEventListener('scroll', scrollWindowHandler);
 
 
 });
